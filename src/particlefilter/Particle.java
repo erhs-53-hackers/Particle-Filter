e838a6d760cfc1592123f022edad259211553e40
Michael Stevens <mls0.5mv2@gmail.com>
2012-03-24 23:58:02 -0700

    complete basic particle filter

--- src/particlefilter/Particle.java
+++ src/particlefilter/Particle.java
@@ -4,6 +4,7 @@
  */
 package particlefilter;
 
+import com.sun.java_cup.internal.runtime.Symbol;
 import java.util.Random;
 
 /**
@@ -12,37 +13,43 @@ import java.util.Random;
  */
 public class Particle {
     public float orientation, forwardNoise, turnNoise, senseNoise;
-    public Point location;
+    public float x, y;
+    public int width;
+    public int height;
     public int worldSize;
+    public double probability = 0;
     public Point[] landmarks;
     Random gen;
 
-    public Particle(Point[] landmarks, int worldSize) {
+    public Particle(Point[] landmarks, int width, int height) {
         gen = new Random();
         this.landmarks = landmarks;
-        this.worldSize = worldSize; 
-        location = new Point();
-        location.x = gen.nextFloat() * this.worldSize;
-        location.y = gen.nextFloat() * this.worldSize;        
+        this.width = width;
+        this.height = height;
+        this.worldSize = width * height;
+        
+        x = gen.nextFloat() * width;
+        y = gen.nextFloat() * height;        
         orientation = gen.nextFloat() * 2f * ((float)Math.PI);
         forwardNoise = 0f;
         turnNoise = 0f;
         senseNoise = 0f;        
     }
     
-    public void set(float x, float y, float orientation) throws Exception {
-        if(x < 0 || x >= worldSize) {
+    public void set(float x, float y, float orientation, double prob) throws Exception {
+        if(x < 0 || x >= width) {
             throw new Exception("X coordinate out of bounds");
         }
-        if(y < 0 || y >= worldSize) {
+        if(y < 0 || y >= height) {
             throw new Exception("Y coordinate out of bounds");
         }
         if(orientation < 0 || orientation >= 2 * Math.PI) {
             throw new Exception("X coordinate out of bounds");
         }
-        location.x = x;
-        location.y = y;
+        this.x = x;
+        this.y = y;
         this.orientation = orientation;
+        this.probability = prob;
     }
     
     public void setNoise(float Fnoise, float Tnoise, float Snoise) {
@@ -55,14 +62,13 @@ public class Particle {
         float[] ret = new float[landmarks.length];
         
         for(int i=0;i<landmarks.length;i++){
-            float dist = (float) distance(location.x, landmarks[i].x, location.y, landmarks[i].y);
+            float dist = (float) distance(x, landmarks[i].x, y, landmarks[i].y);
             ret[i] = dist + (float)gen.nextGaussian() * senseNoise;
         }       
         return ret;
     }    
     
     public void move(float turn, float forward) throws Exception {
-        
         if(forward < 0) {
             throw new Exception("Robot cannot move backwards");
         }
@@ -70,18 +76,21 @@ public class Particle {
         orientation = circle(orientation, 2f * (float)Math.PI);
         
         double dist = forward + gen.nextGaussian() * forwardNoise;
-        location.x += Math.cos(orientation) * dist;
-        location.y += Math.sin(orientation) * dist;
-        location.x = circle(location.x, worldSize);
-        location.y = circle(location.y, worldSize);      
+        
+        x += Math.cos(orientation) * dist;
+        y += Math.sin(orientation) * dist;
+        x = circle(x, width);
+        y = circle(y, height);
+        
     }
     
     private double Gaussian(double mu, double sigma, double x) {       
         return Math.exp(-(Math.pow(mu - x, 2)) / Math.pow(sigma, 2) / 2.0) / Math.sqrt(2.0 * Math.PI * Math.pow(sigma, 2));
     }
     
-    private double distance(float x1, float x2, float y1, float y2) {
-        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y2 - y2, 2));
+    public static double distance(float x1, float y1, float x2, float y2) { 
+        
+        return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
     }
     
     private float circle(float num, float length) {         
@@ -90,18 +99,21 @@ public class Particle {
         return num;       
     }
     
-    public double measurementProb(double[] measurement) {
+    public double measurementProb(float[] measurement) {
         double prob = 1.0;
         for(int i=0;i<landmarks.length;i++) {
-            float dist = (float) distance(location.x, landmarks[i].x, location.y, landmarks[i].y);
-            prob *= Gaussian(dist, senseNoise, measurement[i]);
-        }
+            float dist = (float) distance(x, y, landmarks[i].x, landmarks[i].y);            
+            prob *= Gaussian(dist, senseNoise, measurement[i]);            
+        }      
+        
+        probability = prob;
+        
         return prob;
     }
 
     @Override
     public String toString() {
-        return "[x=" + location.x + " y=" + location.y + " orient=" + orientation;
+        return "[x=" + x + " y=" + y + " orient=" + orientation + " prob=" +probability +  "]";
     }
     
     
--- src/particlefilter/ParticleFilter.java
+++ src/particlefilter/ParticleFilter.java
@@ -15,31 +15,139 @@ public class ParticleFilter {
     /**
      * @param args the command line arguments
      */
+    int numOfParticles = 0;
+    Random gen = new Random();
+    Particle[] particles;
+
+    public ParticleFilter(int numOfParticles, Point[] landmarks, int width, int height) {
+        this.numOfParticles = numOfParticles;
+
+        particles = new Particle[numOfParticles];
+        for (int i = 0; i < numOfParticles; i++) {
+            particles[i] = new Particle(landmarks, width, height);
+        }
+    }
+
+    public void setNoise(float Fnoise, float Tnoise, float Snoise) {
+        for (int i = 0; i < numOfParticles; i++) {
+            particles[i].setNoise(Fnoise, Tnoise, Snoise);
+        }
+    }
+
+    public void move(float turn, float forward) throws Exception {
+        for (int i = 0; i < numOfParticles; i++) {
+            particles[i].move(turn, forward);
+        }
+    }
+
+    public Particle getBestParticle() {
+        Particle particle = particles[0];
+        for (int i = 0; i < numOfParticles; i++) {
+            if (particles[i].probability > particle.probability) {
+                particle = particles[i];
+            }
+        }
+        return particle;
+    }
+
+    public void resample(float[] measurement) throws Exception {
+        Particle[] new_particles = new Particle[numOfParticles];
+
+        for (int i = 0; i < numOfParticles; i++) {
+            particles[i].measurementProb(measurement);
+        }
+        float B = 0f;
+        Particle best = getBestParticle();
+        int index = (int) gen.nextFloat() * numOfParticles;
+        for (int i = 0; i < numOfParticles; i++) {
+            B += gen.nextFloat() * 2f * best.probability;
+            while (B > particles[index].probability) {
+                B -= particles[index].probability;
+                index = circle(index + 1, numOfParticles);
+            }
+            new_particles[i] = new Particle(particles[index].landmarks, particles[index].width, particles[index].height);
+            new_particles[i].set(particles[index].x, particles[index].y, particles[index].orientation, particles[index].probability);
+            new_particles[i].setNoise(particles[index].forwardNoise, particles[index].turnNoise, particles[index].senseNoise);
+        }
+
+        particles = new_particles;        
+    }
+
+    private int circle(int num, int length) {
+        while (num > length - 1) {
+            num -= length;
+        }
+        while (num < 0) {
+            num += length;
+        }
+        return num;
+    }
+
+    @Override
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < numOfParticles; i++) {
+            res += particles[i].toString() + "\n";
+        }
+        return res;
+    }
+
+    public static double Gaussian(double mu, double sigma, double x) {
+        return Math.exp(-(Math.pow(mu - x, 2)) / Math.pow(sigma, 2) / 2.0) / Math.sqrt(2.0 * Math.PI * Math.pow(sigma, 2));
+    }
+    
+
     public static void main(String[] args) throws Exception {
         
-       
-        int worldSize = 10;
-        int N = 5;
-        Particle[] parts = new Particle[N];
-        Point[] landmarks = new Point[] {new Point(0, 0), new Point(100,100)};
-        for(int i=0;i<N;i++) {
-            parts[i] = new Particle(landmarks, worldSize);
-            parts[i].setNoise(.00001f, .000001f, .000001f);            
-        }
+
+        Point[] landmarks = new Point[]{new Point(20f, 20f), new Point(80f, 80f), new Point(20f, 80f), new Point(80f, 20f)};
         
-        for(Particle p : parts) {
-            System.out.println(p.toString());
-        }
+      
+        float[] Z = new float[]{0, 84.8528f, 60, 60};
+        System.out.println("Starting");
+        ParticleFilter filter = new ParticleFilter(1000, landmarks, 100, 100);
+        filter.setNoise(0.05f,  0.05f, 5f);
         
-        for(int i=0;i<N;i++) {
-            parts[i].move(0f, 5f);
-        }
-        System.out.println("-------------------------");
+        //filter.move(.1f, 5f);
         
-        for(Particle p : parts) {
-            System.out.println(p.toString());
-        }
+        System.out.println("sample...");
+        filter.resample(Z);
+        System.out.println(filter.toString());
+        filter.setNoise(0.05f,  0.05f, 5f);
+        filter.move(0f, 5f);
         
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println(filter.toString());
         
+        System.out.println("done!");
+
+        
+
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("-----------");
+        System.out.println("best: "
+                + filter.getBestParticle().toString());
+               
+                
+        
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
