6b9d4b77b16f98f413c219ab8eaf31f16d4268f7
Nick Yaculak <nyaculak@gmail.com>
2013-04-01 11:10:09 -0400

    minor adjustments to sampling size

--- src/particlefilter/GUI.java
+++ src/particlefilter/GUI.java
@@ -13,27 +13,25 @@ import java.util.logging.Logger;
  * @author Michael, Nick
  */
 public class GUI extends javax.swing.JFrame {
-    
+
     ParticleFilter filter;
     Particle robot;
-    final Point[] landmarks = new Point[]{new Point(5, 20f), new Point(225, 40f), 
-                                          new Point(225f, 400f), new Point(5f, 80f), 
-                                          new Point(700-10f,40f), new Point(17f, 92f)};
-    final int NUM_PARTICLES = 2000; 
+    final Point[] landmarks = new Point[]{new Point(5, 20f), new Point(225, 40f),
+                                          new Point(225f, 400f), new Point(5f, 80f),
+                                          new Point(700 - 10f, 40f), new Point(17f, 92f)};
+    final int NUM_PARTICLES = 10000;
     final int WORLD_WIDTH = 700, WORLD_HEIGHT = 500;
-    
     private Image image;
     private Graphics2D graphics;
 
     private void setUp() {
         image = new BufferedImage(WORLD_WIDTH, WORLD_HEIGHT, BufferedImage.BITMASK);
-        
-        graphics = (Graphics2D)image.getGraphics();
+
+        graphics = (Graphics2D) image.getGraphics();
         filter = new ParticleFilter(NUM_PARTICLES, landmarks, WORLD_WIDTH, WORLD_HEIGHT);
         filter.setNoise(0.05f, 0.05f, 5f);
         robot = new Particle(landmarks, WORLD_WIDTH, WORLD_HEIGHT);
-        graphics.drawRect(0, 0, WORLD_WIDTH-1, WORLD_HEIGHT-1);
-        
+        graphics.drawRect(0, 0, WORLD_WIDTH - 1, WORLD_HEIGHT - 1);
     }
 
     public GUI() {
@@ -52,32 +50,32 @@ public class GUI extends javax.swing.JFrame {
         /* Draw simulation environment ****************************************/
         graphics.clearRect(0, 0, WORLD_WIDTH, WORLD_HEIGHT);
         graphics.setPaint(Color.white);
-        graphics.drawRect(0, 0, WORLD_WIDTH-1, WORLD_HEIGHT-1);
+        graphics.drawRect(0, 0, WORLD_WIDTH - 1, WORLD_HEIGHT - 1);
         /* Draw all particles *************************************************/
         graphics.setPaint(Color.PINK);
-        for(Particle p : filter.particles) {
-            graphics.drawRect((int)p.x, (int)p.y, 1, 1);
+        for (Particle p : filter.particles) {
+            graphics.drawRect((int) p.x, (int) p.y, 1, 1);
         }
         /* Draw all landmarks *************************************************/
         graphics.setPaint(Color.RED);
-        for(Point p : landmarks) {
-            graphics.fillOval((int)p.x-10, (int)p.y-10, 20, 20);
+        for (Point p : landmarks) {
+            graphics.fillOval((int) p.x - 10, (int) p.y - 10, 20, 20);
         }
         /* Draw the robot *****************************************************/
         graphics.setPaint(Color.YELLOW);
-        graphics.fillOval((int)robot.x-2, (int)robot.y-2, 4, 4);
+        graphics.fillOval((int) robot.x - 2, (int) robot.y - 2, 4, 4);
         /* Show the best particle *********************************************/
         Particle p = filter.getBestParticle();
         graphics.setPaint(Color.BLUE);
-        graphics.fillOval((int)p.x-5, (int)p.y-5, 10, 10);
+        graphics.fillOval((int) p.x - 5, (int) p.y - 5, 10, 10);
         /* Show the average particle ******************************************/
         p = filter.getAverageParticle();
         graphics.setPaint(Color.GREEN);
-        graphics.fillOval((int)p.x-5, (int)p.y-5, 10, 10);
-        
+        graphics.fillOval((int) p.x - 5, (int) p.y - 5, 10, 10);
+
         g.drawImage(image, 10, 40, rootPane);
     }
-    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -177,11 +175,9 @@ public class GUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
-        // TODO add your handling code here:
     }//GEN-LAST:event_formWindowActivated
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
-       
     }//GEN-LAST:event_formWindowOpened
 
     private void formMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseClicked
@@ -191,7 +187,7 @@ public class GUI extends javax.swing.JFrame {
             robot.set(x, y, 0, 0);
             float[] Z = robot.sense();
             String s = "[";
-            for(float f : Z) {
+            for (float f : Z) {
                 s += f + ", ";
             }
             s += "]";
@@ -215,7 +211,7 @@ public class GUI extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         float move = Float.parseFloat(jTextField1.getText());
-        float turn = (float)Math.toRadians(Float.parseFloat(jTextField2.getText()));
+        float turn = (float) Math.toRadians(Float.parseFloat(jTextField2.getText()));
         try {
             robot.move(turn, move);
             filter.move(turn, move);
@@ -261,7 +257,6 @@ public class GUI extends javax.swing.JFrame {
          * Create and display the form
          */
         java.awt.EventQueue.invokeLater(new Runnable() {
-
             public void run() {
                 new GUI().setVisible(true);
             }
